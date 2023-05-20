c862cbd4827a399ca57b45334fcd3312f3095c13
ManchalaTejaSwaroop <101047476+ManchalaTejaSwaroop@users.noreply.github.com>
2023-05-20 13:29:33 +0530

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int c;
+   cout<<"enter a number:";
+   cin>>c;
+   cout<<"cube of the number is :"<<c*c*c;
+    return (0);
+}
---
+++ 6.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	 int d=3,e=5,f;
+	  cout<<"d:"<<d<<" e:"<<e<<endl;
+	  f=d;
+	  d=e;
+	  e=f;
+	  cout<<"d:"<<d<<" e:"<<e;      
+	return (0);
+}
